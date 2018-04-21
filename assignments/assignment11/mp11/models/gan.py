008bd28696bc4b4b9cbf200d68dd3b68f9386137
Zayne <32248549+Zhenye-Na@users.noreply.github.com>
2018-04-20 20:41:46 -0500

    Add Gan

--- assignments/assignment11/mp11/models/gan.py
+++ assignments/assignment11/mp11/models/gan.py
@@ -6,17 +6,17 @@ import tensorflow as tf
 from tensorflow import contrib
 from tensorflow.contrib import layers
 
+
 class Gan(object):
-    """Adversary based generator network.
-    """
+    """Adversary based generator network."""
+
     def __init__(self, ndims=784, nlatent=2):
-        """Initializes a GAN
+        """Initialize a GAN.
 
         Args:
             ndims(int): Number of dimensions in the feature.
             nlatent(int): Number of dimensions in the latent space.
         """
-
         self._ndims = ndims
         self._nlatent = nlatent
 
@@ -38,66 +38,137 @@ class Gan(object):
         self.g_loss = self._generator_loss(y_hat)
 
         # Add optimizers for appropriate variables
+        self.learning_rate_placeholder = tf.placeholder(tf.float32, [])
+        self.d_optimizer = tf.train.GradientDescentOptimizer(
+            learning_rate=self.learning_rate_placeholder, name='d_optimizer').minimize(self.d_loss)
+        self.g_optimizer = tf.train.GradientDescentOptimizer(
+            learning_rate=self.learning_rate_placeholder, name='g_optimizer').minimize(self.g_loss)
 
         # Create session
         self.session = tf.InteractiveSession()
         self.session.run(tf.global_variables_initializer())
-
 
     def _discriminator(self, x, reuse=False):
         """Discriminator block of the network.
 
         Args:
             x (tf.Tensor): The input tensor of dimension (None, 784).
-            reuse (Boolean): re use variables with same name in scope instead of creating
-              new ones, check Tensorflow documentation
+            reuse (Boolean): re use variables with same name in scope instead
+                of creating new ones, check Tensorflow documentation
         Returns:
-            y (tf.Tensor): Scalar output prediction D(x) for true vs fake image(None, 1). 
-              DO NOT USE AN ACTIVATION FUNCTION AT THE OUTPUT LAYER HERE.
+            y (tf.Tensor): Scalar output prediction D(x) for true vs fake
+                image(None, 1).
+
+        DO NOT USE AN ACTIVATION FUNCTION AT THE OUTPUT LAYER HERE.
 
         """
         with tf.variable_scope("discriminator", reuse=reuse) as scope:
-            y = None
-            return y
+            if reuse:
+                scope.reuse_variables()
 
+            # Input Layer
+            inputs = layers.fully_connected(
+                inputs=x, num_outputs=512, activation_fn=tf.nn.relu)
+
+            # Hidden Layer 1
+            hidden1 = layers.fully_connected(
+                inputs=inputs, num_outputs=256, activation_fn=tf.nn.relu)
+
+            # Drop Out
+            drop_out = tf.layers.dropout(
+                inputs=hidden1, rate=0.15, training=True)
+
+            # Hidden Layer 2
+            hidden2 = layers.fully_connected(
+                inputs=drop_out, num_outputs=64, activation_fn=tf.nn.relu)
+
+            # Output Layer
+            y = layers.fully_connected(
+                inputs=hidden2, num_outputs=1, activation_fn=None)
+
+            return y
 
     def _discriminator_loss(self, y, y_hat):
         """Loss for the discriminator.
 
         Args:
-            y (tf.Tensor): The output tensor of the discriminator for true images of dimension (None, 1).
-            y_hat (tf.Tensor): The output tensor of the discriminator for fake images of dimension (None, 1).
+            y (tf.Tensor): The output tensor of the discriminator for true
+                images of dimension (None, 1).
+            y_hat (tf.Tensor): The output tensor of the discriminator for fake
+                images of dimension (None, 1).
         Returns:
             l (tf.Scalar): average batch loss for the discriminator.
 
         """
-        l = 0
-        return l
+        l = tf.reduce_mean(tf.reduce_sum(
+            tf.nn.sigmoid_cross_entropy_with_logits(labels=y,
+                                                    logits=y_hat,
+                                                    name="d_loss"), 1))
 
+        # l = tf.reduce_mean(tf.reduce_sum(tf.log(y) + tf.log(1 - y_hat), 1))
+        return l
 
     def _generator(self, z, reuse=False):
         """From a sampled z, generate an image.
 
         Args:
             z(tf.Tensor): z from _sample_z of dimension (None, 2).
-            reuse (Boolean): re use variables with same name in scope instead of creating
-              new ones, check Tensorflow documentation 
+            reuse (Boolean): re use variables with same name in scope instead
+                of creating new ones, check Tensorflow documentation
         Returns:
             x_hat(tf.Tensor): Fake image G(z) (None, 784).
         """
         with tf.variable_scope("generator", reuse=reuse) as scope:
-            x_hat = None
-            return x_hat
 
+            if reuse:
+                scope.reuse_variables()
+
+            # Input Layer
+            inputs = layers.fully_connected(
+                inputs=z, num_outputs=64, activation_fn=tf.nn.relu)
+
+            # Hidden Layer 1
+            hidden1 = layers.fully_connected(
+                inputs=inputs, num_outputs=256, activation_fn=tf.nn.relu)
+
+            # Drop Out
+            drop_out = tf.layers.dropout(
+                inputs=hidden1, rate=0.15, training=True)
+
+            # Hidden Layer 2
+            hidden2 = layers.fully_connected(
+                inputs=drop_out, num_outputs=512, activation_fn=tf.nn.relu)
+
+            # Output Layer
+            x_hat = layers.fully_connected(
+                inputs=hidden2, num_outputs=self._ndims, activation_fn=tf.nn.sigmoid)
+
+            return x_hat
 
     def _generator_loss(self, y_hat):
         """Loss for the discriminator.
 
         Args:
-            y_hat (tf.Tensor): The output tensor of the discriminator for fake images of dimension (None, 1).
+            y_hat (tf.Tensor): The output tensor of the discriminator for fake
+                images of dimension (None, 1).
         Returns:
             l (tf.Scalar): average batch loss for the discriminator.
 
         """
-        l = 0
+        l = tf.reduce_mean(tf.reduce_sum(y_hat, axis=1, name="g_loss"))
         return l
+
+    def generate_samples(self, z_np):
+        """Generate random samples from the provided z_np.
+
+        Args:
+            z_np(numpy.ndarray): Numpy array of dimension
+                (batch_size, _nlatent).
+
+        Returns:
+            out(numpy.ndarray): The sampled images (numpy.ndarray) of
+                dimension (batch_size, _ndims).
+        """
+        out = self.x_hat.eval(session=self.session, feed_dict={
+                              self.z_placeholder: z_np})
+        return out
